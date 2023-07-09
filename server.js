844c0dd74efa9827d066ae44e18ed9582d038ec3
varun singh <pratap@arpiantech.com>
2023-07-10 03:33:20 +0530

    server update

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 var httpServer = http.createServer(app);
 let PORT = process.env.PORT || 3001;
 
-// const server = https.createServer({
-//     key: fs.readFileSync('../etc/ssl/private.key'), // Your private key
-//     cert: fs.readFileSync('../etc/ssl/certificate.crt') // Your SSL certificate
-//   }, app).listen(PORT);
-const server = httpServer.listen(PORT, () => {
-    console.log(`Server is up and running on ${PORT} ...`);
-})
+const server = https.createServer({
+    key: fs.readFileSync('../../etc/ssl/private.key'), // Your private key
+    cert: fs.readFileSync('../../etc/ssl/certificate.crt') // Your SSL certificate
+  }, app).listen(PORT);
+// const server = httpServer.listen(PORT, () => {
+//     console.log(`Server is up and running on ${PORT} ...`);
+// })
