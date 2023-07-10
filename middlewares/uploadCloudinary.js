17e3fe214469698e5d2e8e734e11920ca54fe1cd
varun singh <pratap@arpiantech.com>
2023-07-10 13:31:36 +0530

    update

--- middlewares/uploadCloudinary.js
+++ middlewares/uploadCloudinary.js
@@ -12,37 +12,71 @@ module.exports = function(req, res, next) {
   try {
     const s3 = new AWS.S3();
     let reqData = req.body;
-    console.log("upload data", reqData); 
-
-    // Iterate over each image
-    let uploadedLocations = [];
-    reqData.productImages.forEach((image, index) => {
-      let pathkey = reqData.images[index].path; 
-      const imageBuffer = Buffer.from(image.base64String, 'base64');
+    
+    if (typeof reqData.cover === "string") {
+      console.log("type of 2", typeof reqData.cover);
+      // next();
+    } else {
+      let pathkey = reqData.cover.path;
+      const imageBuffer = Buffer.from(reqData.coverImage, 'base64');
       const params = {
-          Bucket: config.AWS_BUCKET_NAME,
-          Key: pathkey,
-          Body: imageBuffer,
-          ACL: "public-read-write",
-          ContentType: "image/jpeg"
+          Bucket: config.AWS_BUCKET_NAME,      // bucket that we made earlier
+          Key: pathkey,               // Name of the image
+          Body: imageBuffer,                    // Body which will contain the image in buffer format
+          ACL: "public-read-write",                 // defining the permissions to get the public link
+          ContentType: "image/jpeg"                 // Necessary to define the image content-type to view the photo in the browser with the link
       };
-      
+
       // Upload the file to the S3 bucket
       s3.upload(params, (err, data) => {
-        if (err) {
-          console.log('Error uploading file:', err);
-        } else {
-          console.log('File uploaded successfully:', data.Location);
-          uploadedLocations.push(data.Location);
-          // Check if all images have been uploaded
-          if (uploadedLocations.length === reqData.productImages.length) {
-            // Add the URLs to the request object
-            req.body.icon = JSON.stringify(uploadedLocations);
-            next();
+          if (err) {
+              console.log('Error uploading file:', err);
+          } else {
+              req.body.cover = data.Location;
+              // next();
           }
-        }
       });
-    });
+    }
+
+    if (reqData.images.length > 0 ) {
+      if(typeof reqData.images[0] === "string"){
+        req.body.images = JSON.stringify(reqData.images);
+        next();
+      }else{
+        // Iterate over each image
+        let uploadedLocations = [];
+        reqData.productImages.forEach((image, index) => {
+          let pathkey = reqData.images[index].path; 
+          const imageBuffer = Buffer.from(image.base64String, 'base64');
+          const params = {
+              Bucket: config.AWS_BUCKET_NAME,
+              Key: pathkey,
+              Body: imageBuffer,
+              ACL: "public-read-write",
+              ContentType: "image/jpeg"
+          };
+          
+          // Upload the file to the S3 bucket
+          s3.upload(params, (err, data) => {
+            if (err) {
+              console.log('Error uploading file:', err);
+            } else {
+              console.log('File uploaded successfully:', data.Location);
+              uploadedLocations.push(data.Location);
+              // Check if all images have been uploaded
+              if (uploadedLocations.length === reqData.productImages.length) {
+                // Add the URLs to the request object
+                req.body.images = JSON.stringify(uploadedLocations);
+                next();
+              }
+            }
+          });
+        });
+      }     
+    } 
+
+    
+    
   } catch(err) {
     console.log('Error uploading file:', err);
   }
--- routes/product.js
+++ routes/product.js
@@ -36,134 +36,105 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // support parsing of application/json type post data
 app.use(cookieParser());
 
+// Define the GET API for products
+app.get('/products', async (req, res) => {
+  try {
+    // make sure that any items are correctly URL encoded in the connection string     
+    let result;
+    let queryStr;
+    // let expert = req.decoded.user;
+
+
+    queryStr = `SELECT * FROM products`;
 
 
 
-const mockProducts = [
-    {
-      id: "1",
-      title: 'Foam Padded Chair',
-      star: 4.5,
-      sold: 8374,
-      price: 120.00,
-      icon: 'https://res.cloudinary.com/dsty70mlq/image/upload/v1684343987/foam_padded_chair_2x_rzevjg.png',
-    },
-    {
-      id: "2",
-      title: 'Small Bookcase',
-      star: 4.7,
-      sold: 7483,
-      price: 145.40,
-      icon: 'https://res.cloudinary.com/dsty70mlq/image/upload/v1684343987/book_case_2x_qq8wq2.png',
-    },
-    {
-      id: "3",
-      title: 'Glass Lamp',
-      star: 4.3,
-      sold: 6937,
-      price: 40.00,
-      icon: 'https://res.cloudinary.com/dsty70mlq/image/upload/v1684343987/lamp_s7ikym.png',
-    },
-    {
-      id: "4",
-      title: 'Glass Package',
-      star: 4.9,
-      sold: 8174,
-      price: 55.00,
-      icon: 'https://res.cloudinary.com/dsty70mlq/image/upload/v1684343987/class_package_2x_aaoy78.png',
-    },
-    {
-      id: "5",
-      title: 'Plastic Chair',
-      star: 4.6,
-      sold: 6843,
-      price: 65.00,
-      icon: 'https://res.cloudinary.com/dsty70mlq/image/upload/v1684343987/plastic_chair_2x_dytymk.png',
-    },
-    {
-      id: "6",
-      title: 'Wooden Chairs',
-      star: 4.5,
-      sold: 7758,
-      price: 69.00,
-      icon: 'https://res.cloudinary.com/dsty70mlq/image/upload/v1684343987/wooden_chairs_szl9th.png',
-    },
-    // Add more products as needed...
-  ];
-  
-  // Define the GET API for products
-  app.get('/products', async  (req, res) => {
-
-    try {
-        // make sure that any items are correctly URL encoded in the connection string     
-        let result;
-        let queryStr;
-        // let expert = req.decoded.user;
-
-        
-        queryStr = `SELECT * FROM products`;
-        
-        
-            
-        await connection.query(queryStr, async function (error, results, fields) {
-            // console.log(error, results);
-            if (error){
-                // console.log("error", error);
-                res.send({ message:"error", err:error });
-            }else {                
-                result =JSON.parse(JSON.stringify(results));             
-                res.send({ status: true, data: result});
-            }
-        });
-    } catch (err) {
-        // ... error checks
-        console.log("errornew", err);
-        res.send(err);
-    }
-    // res.send({ status: true, data: mockProducts});
-  });
-
-  app.post('/products', upload, (req, res) => {
-    let product = req.body;
-    console.log("products", product);
-    var sqlQuery = 'INSERT INTO `products` (`title`,`description`,`star`,`sold`,`price`,`icon`,code, sku, priceSale, taxes, inStock `created_at`,`updated_at`) VALUES (?,?,?,?,?,?,?,?)';
-    var values = [product.title, product.description, 4.5, 231, product.price, product.icon, product.code, product.sku, product.priceSale, product.taxes, product.inStock, product.created_at, product.updated_at];
-
-    connection.query(sqlQuery, values, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Product inserted...');
+    await connection.query(queryStr, async function (error, results, fields) {
+      // console.log(error, results);
+      if (error) {
+        // console.log("error", error);
+        res.send({ message: "error", err: error });
+      } else {
+        result = JSON.parse(JSON.stringify(results));
+        res.send({ status: true, data: result });
+      }
     });
+  } catch (err) {
+    // ... error checks
+    console.log("errornew", err);
+    res.send(err);
+  }
+  // res.send({ status: true, data: mockProducts});
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const queryStr = `SELECT * FROM products WHERE id = ?`;
+    
+    await connection.query(queryStr, [id], function (error, results, fields) {
+      if (error) {
+        res.send({ message: "error", err: error });
+      } else {
+        const result = JSON.parse(JSON.stringify(results));
+        // Check if product exists
+        if (result.length > 0) {
+          res.send({ status: true, product: result[0] });
+        } else {
+          res.send({ status: false, message: "No product found with the provided id" });
+        }
+      }
+    });
+  } catch (err) {
+    console.log("errornew", err);
+    res.send(err);
+  }
+});
+
+app.post('/products',  upload, (req, res) => {
+  let product = req.body;  
+  var sqlQuery = 'INSERT INTO `products` (`title`,`description`,`star`,`sold`,`price`,`cover`, `images`, `code`, `sku`, `priceSale`, `taxes`, `inStock`, `created_at`,`updated_at`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+  var values = [product.title, product.description, 4.5, 231, product.price, product.cover, product.images, product.code, product.sku, product.priceSale, product.taxes, product.inStock, new Date().toJSON().slice(0, 19).replace('T', ' '), new Date().toJSON().slice(0, 19).replace('T', ' ')];
+
+  connection.query(sqlQuery, values, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('Product inserted...');
   });
+});
 
 // Update product
-app.put('/products/:id', async (req, res) => {
-    const { title, description, star, sold, price, icon } = req.body;
-    const { id } = req.params;
-    try {
-        const [rows, fields] = await connection.query('UPDATE Products SET title = ?, description = ?, star = ?, sold = ?, price = ?, icon = ? WHERE id = ?', [title, description, star, sold, price, icon, id]);
-        res.json({ message: "Product updated successfully" });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+app.put('/products/:id', upload, (req, res) => {
+  let product = req.body;
+  const { id } = req.params;
+
+  var sqlQuery = 'UPDATE `products` SET `title`=?, `description`=?, `star`=?, `sold`=?, `price`=?, `cover`=?, `images`=?, `code`=?, `sku`=?, `priceSale`=?, `taxes`=?, `inStock`=?, `updated_at`=? WHERE `id`=?';
+  
+  var values = [product.title, product.description, 4.5, 231, product.price, product.cover, product.images, product.code, product.sku, product.priceSale, product.taxes, product.inStock, new Date().toJSON().slice(0, 19).replace('T', ' '), id];
+
+  connection.query(sqlQuery, values, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('Product updated...');
+  });
 });
 
 // Delete product
 app.delete('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    console.log("delete ", id);
-    try {
+  const { id } = req.params;
+  console.log("delete ", id);
+  try {
 
-      connection.query('DELETE FROM Products WHERE id = ?', [id], (err, result) => {
-          if(err) throw err;
-          console.log(result);
-          res.send({ message: "Product deleted successfully" });
-      });
-        
-       
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+    connection.query('DELETE FROM Products WHERE id = ?', [id], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send({ message: "Product deleted successfully" });
+    });
+
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 
