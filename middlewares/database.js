def5d20a5b9c50a756db31a7f8e7c01c8dc8ba33
varun singh <pratap@arpiantech.com>
2024-07-27 01:32:43 +0530

    changes

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,36 +1,78 @@
-var mysql      = require('mysql');
-const fs = require('fs')
+// var mysql      = require('mysql');
+// const fs = require('fs')
+// const config = require("../key");
+
+
+
+// var connection = mysql.createConnection({
+//     host     : config.host,
+//     user     : config.user,
+//     password : config.password,
+//     port: config.port,
+//     connectionLimit: 15,
+//     queueLimit: 30,
+//     acquireTimeout: 1000000,
+//     database : config.database,
+    
+   
+//   });
+
+
+
+
+// connection.connect((error) => {
+//     if (error) {
+//         console.error('Error connecting to MySQL server: ', error);
+//         return;
+//     }
+//     console.log('Connected to MySQL server.');
+// });
+
+
+
+// // console.log('Connected to MsSQL server', connection)
+
+
+// module.exports = connection;
+
+
+var mysql = require('mysql');
 const config = require("../key");
 
+let connection;
 
-
-var connection = mysql.createConnection({
-    host     : config.host,
-    user     : config.user,
-    password : config.password,
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
     port: config.port,
     connectionLimit: 15,
     queueLimit: 30,
     acquireTimeout: 1000000,
-    database : config.database,
-    
-   
+    database: config.database,
   });
 
-
-
-
-connection.connect((error) => {
+  connection.connect((error) => {
     if (error) {
-        console.error('Error connecting to MySQL server: ', error);
-        return;
+      console.error('Error connecting to MySQL server: ', error);
+      setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
+    } else {
+      console.log('Connected to MySQL server.');
     }
-    console.log('Connected to MySQL server.');
-});
+  });
 
+  connection.on('error', (err) => {
+    console.error('MySQL error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
+      console.log('Attempting to reconnect...');
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
 
-
-// console.log('Connected to MsSQL server', connection)
-
+handleDisconnect();
 
 module.exports = connection;
